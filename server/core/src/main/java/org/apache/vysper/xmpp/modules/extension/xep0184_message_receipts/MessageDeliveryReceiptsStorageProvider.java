af3953794244991d52ea9cbb072fd711fac3e4bc
Roger Chang <roger@sonar.me>
2012-10-17 17:43:23 -0400

    adding new messagedelivery extension

---
+++ server/core/src/main/java/org/apache/vysper/xmpp/modules/extension/xep0184_message_receipts/MessageDeliveryReceiptsStorageProvider.java
@@ -0,0 +1,12 @@
+package org.apache.vysper.xmpp.modules.extension.xep0184_message_receipts;
+
+import org.apache.vysper.storage.StorageProvider;
+import org.apache.vysper.xmpp.stanza.Stanza;
+
+import java.util.List;
+
+public interface MessageDeliveryReceiptsStorageProvider extends StorageProvider {
+
+    public boolean confirmMessageDelivery(String bareJID, List<? extends Stanza> messages);
+
+}
